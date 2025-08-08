27118e9346cb036a0dfc5090e3424b013a157f54
saed abbas <saedabbas0@gmail.com>
2025-08-08 14:03:52 +0300

    new commit

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,18 +1,38 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import React from "react";
 
 const Document = () => {
   return (
-    <div className=''>
+    <div className="">
       <Header>
         <div className="flex w-fit items-center justify-center gap-2">
-          <p className='document-title'>This is a <br/> fake document title </p> 
+          <p className="document-title">This is a fake document title </p>
+        </div>
+        <div className="">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </Header>
       <Editor />
     </div>
-  )
-}
+  );
+};
 
-export default Document
+export default Document;
