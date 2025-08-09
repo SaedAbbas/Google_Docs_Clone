3257ee2a885235edd9026a5ce12b84848160064f
saed abbas <saedabbas0@gmail.com>
2025-08-09 16:55:19 +0300

    new commit

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from 'react'
 
 const Provider = ({children}:{children:ReactNode}) => {
   return (
+    //السبب إنه استخدم authEndpoint بدل المفتاح العام هو الأمان، عشان ما ينكشف مفتاحك السري في المتصفح.
     <LiveblocksProvider authEndpoint='/api/liveblocks-auth'>
         <ClientSideSuspense fallback={<Loader />}>
           {children}
