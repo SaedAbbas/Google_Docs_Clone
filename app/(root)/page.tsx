1f28355f5034acbbc086868ff20ce08c37e62d17
saed abbas <saedabbas0@gmail.com>
2025-08-10 15:30:46 +0300

    new commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,32 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/Header";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home() {
+  const documents = [];
   return (
-    <div className="flex justify-center items-center p-6 bg-white">
-      <Button>saed</Button>
-
-    </div>
-  )
+    <main className="home-container">
+      <Header className="sticky top-0 left-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+      {documents.length > 0 ? (
+        <div className=""></div>
+      ) : (
+        <div className="document-list-empty">
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="docs"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+        </div>
+      )}
+    </main>
+  );
 }
