8b7ec9e2e44de9876cdee6dfa1c5498e60c896cc
saed abbas <saedabbas0@gmail.com>
2025-08-12 14:59:13 +0300

    new commit

--- lib/actions/room.action.ts
+++ lib/actions/room.action.ts
@@ -5,6 +5,7 @@ import { revalidatePath } from "next/cache";
 import { parseStringify } from "../utils";
 
 //Set permission accesses to a room after creating a document  //this code server action
+
 export const createDocument = async ({
   userId,
   email,
@@ -17,20 +18,29 @@ export const createDocument = async ({
       email,
       title: "untitled",
     };
-
+    
+    // جهزت بيانات وصفية (metadata) عن الغرفة، زي مين عملها والإيميل والعنوان.
+    
     const usersAccesses : RoomAccesses = {
-        [email]: ['room:write']
+      [email]: ['room:write']
     }
-
+    
     const room = await liveblocks.createRoom(roomId, {
-        metadata,
-        usersAccesses,
-        defaultAccesses:[]
+      metadata,
+      usersAccesses,
+      defaultAccesses:[]
     });
-    revalidatePath('/')
-
+    revalidatePath('/') //بعد ما الغرفة اتعملت، طلبت من Next.js يعمل تحديث للصفحة الرئيسية / عشان يظهر فيها التغييرات.
+    
     return parseStringify(room)  //export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
   } catch (error) {
     console.log("error happend while creating a room", error);
   }
 };
+
+/*
+لو ما عملتها Server Action وشغلتها في Client Component مباشرة؟
+هيحصل خطأ لأن @liveblocks/node مكتبة خاصة بالسيرفر (تستخدم مفاتيح سرية وغير مصممة للكلاينت).
+
+لذلك لازم تكون الدالة دي موجودة فقط في السيرفر (Server Action أو API Route).
+*/
