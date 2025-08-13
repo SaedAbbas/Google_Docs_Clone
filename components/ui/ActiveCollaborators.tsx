2bdb85aaf7a7781ea6fb83e509a53c93a804b576
saed abbas <saedabbas0@gmail.com>
2025-08-13 15:30:33 +0300

    new commit

--- components/ui/ActiveCollaborators.tsx
+++ components/ui/ActiveCollaborators.tsx
@@ -8,18 +8,19 @@ const ActiveCollaborators = () => {
 
   return (
     <ul className="collaborators-list">
-      {collaborators.map(({ id, avatar, name, color }) => 
+      {collaborators.map(({ id, avatar, name, color }) => (
         <li key={id}>
           <Image
+            priority
             src={avatar}
             alt={name}
             width={100}
             height={100}
             className="inline-block size-8 rounded-full ring-2 ring-dark-100"
-            style={{border:`3px solid ${color}`}}
+            style={{ border: `3px solid ${color}` }}
           />
         </li>
-      )}
+      ))}
     </ul>
   );
 };
